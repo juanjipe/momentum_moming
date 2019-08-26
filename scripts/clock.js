f7daa369956f0060dc4a89058352a7690ec24fd5
juanjipe <54365305+juanjipe@users.noreply.github.com>
2019-08-26 11:59:19 -0400

    make clock

---
+++ scripts/clock.js
@@ -0,0 +1,16 @@
+const clockContainer = docment.querySelector(.js-clock);
+const clockTitle = clockContainer.querySelector("");
+
+function getTime() {
+    const date = new Date ();
+    const hours = date.getUTCHours();
+    const minutes = date.getUTCMinutes();
+    const seconds = date.getSseconds();
+
+    clockTitle.innerText `${hours}: ${minutes} $ {seconds}`; 
+}
+function init() {
+    getTime();
+    setInterval(getTime, 1000);
+}
+init();
